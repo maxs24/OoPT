33defcaaf840684f182ae930b515b3ae11e39fac
Айдар <aidar.nur@hotmail.com>
2021-02-17 14:48:01 +0300

    Начал свою ветку, написал начальную часть клиента.

--- Client/src/Client.kt
+++ Client/src/Client.kt
@@ -1,7 +1,15 @@
+import com.kfu.imim.components.window
+import com.kfu.imim.networking.Communicator
 import com.kfu.imim.utils.Balancer
 import com.kfu.imim.utils.Recommendations
 import com.kfu.imim.utils.TaskInstance
 import com.kfu.imim.utils.TaskInstanceCreateStructure
+import java.net.Socket
+
+enum class UserType{
+    Customer,
+    Executor
+}
 
 class Client private constructor() {
     companion object {
@@ -11,9 +19,40 @@ class Client private constructor() {
             return client
         }
     }
+    val serverAdress = "localhost"
+    val port = 5703
+    //val socket = Socket(serverAdress, port)
     var recommendations: Recommendations? = null
+    //val communicator = Communicator(socket)
 
-    private fun submitTask() {
+    init {
+        //communicator.addDataRecievedListener(::dataReceived)
+    }
+
+    private fun dataReceived (data: String) {
+        val vls = data.split("=", limit = 2)
+        if (vls.size == 2){
+            when (vls[0]){ //when - это как switch на с++
+                "login" -> loginResponse(vls[1])
+            }
+        }
+    }
+
+    fun loginResponse(string: String) {
+        val vls = string.split(":")
+        try{
+            when(vls[0]){
+                "ok" -> { window.chooseUserType(UserType.valueOf(vls[1]), vls[2])}
+                else -> { }
+            }
+        }
+        catch (ex: IllegalArgumentException) {
+            println("Не сработал valueOf $ex")
+        }
+
+    }
+
+    fun submitTask() {
         //действие, вызываемое при нажатии кнопки "Отправить". Создаёт задачу с заданными в форме параметрами
         // само собой такая кнопка есть только у заказчиков
         val taskInstanceCreateStructure: TaskInstanceCreateStructure = TaskInstanceCreateStructure()
@@ -21,8 +60,19 @@ class Client private constructor() {
         val taskInstance = TaskInstance(taskInstanceCreateStructure)
     }
 
-    private fun getRecommendations() { //Получить рекомендации по текущей задаче. Обязательно вызвать хотя бы раз
+    fun getRecommendations(taskInstanceCreateStructure: TaskInstanceCreateStructure) { //Получить рекомендации по текущей задаче. Обязательно вызвать хотя бы раз
         // при создании задачи, желательно вызывать после каждого обновления информации в форме. Ниже пример
-        recommendations = Balancer.balance(TaskInstance(TaskInstanceCreateStructure()))
+        recommendations = Balancer.balance(taskInstanceCreateStructure)
+        showRecommendations(recommendations)
+    }
+
+    fun showRecommendations(recommendations: Recommendations?) {
+        if(recommendations == null) return
+
+        //TODO(Показать рекомендации)
+    }
+
+    fun login (login: String, password: String) {
+        //communicator.sendData("login=$login:$password")
     }
 }
